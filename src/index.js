54dffe1f8244953f42a9722ba8a656cd0f0376e3
Sergio Garcia <sgarciaga@indra.es>
2017-03-03 09:07:56 +0100

    Inicio Proyecto

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+/* jshint ignore:start */
+ReactDOM.render(
+  <div/>,
+  document.getElementById('root')
+);
+/* jshint ignore:end */
